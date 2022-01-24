775af9bebf80a74773f0687650b5a3ac224f2012
Rosembergg <rrosembergg.45@gmail.com>
2022-01-24 17:54:55 -0300

    Next aula

---
+++ Captura de entradas/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+ 
+
+namespace Captura_de_entradas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 1;
+            int b = 9;
+            int c = 4;
+            int d = 3;
+            int total = 0;
+
+            total = (a*b)-(c*d);
+            Console.WriteLine($"DIFERENCA = {total}");        
+        }
+    }
+}
